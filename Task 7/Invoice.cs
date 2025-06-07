316961b30a48cc73038b410c1d290be4f61df2ad
Limonchik-D <DavLimon4@yandex.com>
2025-06-07 19:59:04 +0300

    Add files via upload

---
+++ Task 7/Invoice.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_7
+{
+    internal class Invoice
+    {
+        public int Account { get; }      // (Счет) Инициализируется в конструкторе, не изменяется
+        public string Customer { get; } //Покупатель
+        public string Provider { get; } //Поставщик
+
+        private string article;       // Товар
+        private int quantity;      // Количество
+        private double pricePerUnit; // Цена за единицу
+
+        public Invoice(int account, string customer, string provider, string article, int quantity, double pricePerUnit) 
+        {
+            Account = account;
+            Customer = customer;
+            Provider = provider;
+            this.article = article;
+            this.quantity = quantity;
+            this.pricePerUnit = pricePerUnit;
+        }
+
+        // Стоимость без НДС
+        public double CalculatePriceWithoutVAT()
+        {
+            return quantity * pricePerUnit;
+        }
+
+        // Стоимость с НДС (например 20%)
+        public double CalculatePriceWithVAT()
+        {
+            return CalculatePriceWithoutVAT() * 1.20;
+        }
+
+        public void ShowInvoice(bool includeVAT)
+        {
+            Console.WriteLine($"Счёт: {Account}");
+            Console.WriteLine($"Покупатель: {Customer}");
+            Console.WriteLine($"Поставщик: {Provider}");
+            Console.WriteLine($"Товар: {article}");
+            Console.WriteLine($"Количество: {quantity}");
+            Console.WriteLine($"Цена за единицу: {pricePerUnit:F2} BYN");
+
+            if (includeVAT)
+            {
+                Console.WriteLine($"Итого с НДС: {CalculatePriceWithVAT():F2} BYN");
+            }
+            else
+            {
+                Console.WriteLine($"Итого без НДС: {CalculatePriceWithoutVAT():F2} BYN");
+            }
+        }
+    }
+}
---
+++ Task 7/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Invoice invoice = new Invoice(101, "ООО Ромашка", "Завод Цветов", "Цветы", 10, 15.5);
+
+            Console.WriteLine("Вывести сумму с НДС? (да/нет):");
+            string answer = Console.ReadLine()?.ToLower();
+
+            bool withVAT = (answer == "да" || answer == "yes");
+
+            invoice.ShowInvoice(withVAT);
+                
+            Console.ReadKey();
+        }
+    }
+}
