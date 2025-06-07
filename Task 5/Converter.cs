8733a0d1d672579f20909574c00121834636292b
Limonchik-D <DavLimon4@yandex.com>
2025-06-07 19:34:02 +0300

    Add files via upload

---
+++ Task 5/Converter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_5
+{
+    internal class Converter
+    {
+        private double usdRate; // курс USD к BYN
+        private double eurRate; // курс EUR к BYN
+        private double rubRate; // курс RUB к BYN
+
+        public Converter(double usd, double eur, double rub) //Конструктор
+        {
+            usdRate = usd;
+            eurRate = eur;
+            rubRate = rub;
+        }
+        public double FromBYN(double amountBYN, string currency) // Конвертация из BYN в другую валюту
+        {
+            switch (currency.ToUpper())
+            {
+                case "USD":
+                    return amountBYN / usdRate;
+                case "EUR":
+                    return amountBYN / eurRate;
+                case "RUB":
+                    return amountBYN / rubRate;
+                default:
+                    throw new ArgumentException("Неизвестная валюта");
+            }
+        }
+        public double ToBYN(double amountCurrency, string currency) // Конвертация из другой валюты в BYN
+        {
+            switch (currency.ToUpper())
+            {
+                case "USD":
+                    return amountCurrency * usdRate;
+                case "EUR":
+                    return amountCurrency * eurRate;
+                case "RUB":
+                    return amountCurrency * rubRate;
+                default:
+                    throw new ArgumentException("Неизвестная валюта");
+            }
+        }
+    }
+}
---
+++ Task 5/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Converter converter = new Converter(2.5, 3.0, 0.035); // Пример курса: 1 USD = 2.5 BYN, 1 EUR = 3 BYN, 1 RUB = 0.035 BYN
+
+            Console.Write("Введите сумму в BYN для конвертации: ");
+            double amountBYN = double.Parse(Console.ReadLine());
+
+            Console.Write("Введите валюту для конвертации из BYN (USD, EUR, RUB): ");
+            string currency = Console.ReadLine();
+
+            double converted = converter.FromBYN(amountBYN, currency);
+            Console.WriteLine($"{amountBYN} BYN = {converted:F2} {currency.ToUpper()}");
+
+            Console.Write("\nВведите сумму в валюте для конвертации в BYN: ");
+            double amountCurrency = double.Parse(Console.ReadLine());
+
+            Console.Write("Введите валюту (USD, EUR, RUB): ");
+            string currency2 = Console.ReadLine();
+
+            double convertedToBYN = converter.ToBYN(amountCurrency, currency2);
+            Console.WriteLine($"{amountCurrency} {currency2.ToUpper()} = {convertedToBYN:F2} BYN");
+
+            Console.ReadKey();
+        }
+    }
+}
