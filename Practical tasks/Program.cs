a362b2b3896e27cf1ce8e492f9bb17e17a80ce75
Limonchik-D <DavLimon4@yandex.com>
2025-06-07 17:54:41 +0300

    Add files via upload

---
+++ Practical tasks/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical_tasks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите первую сторону прямоугольника: ");
+            double side1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите вторую сторону прямоугольника: ");
+            double side2 = Convert.ToDouble(Console.ReadLine());
+
+            Rectangle rectangle = new Rectangle(side1, side2);
+            Console.WriteLine($"Площадь прямоугольника равна: {rectangle.AreaCalculator()}"); //Через метод
+            Console.WriteLine($"Периметр прямоугольника равна: {rectangle.Perimeter}"); //Через свойство
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Practical tasks/Rectangle.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical_tasks
+{
+    internal class Rectangle
+    {
+        private double side1; //Сторона
+        private double side2;
+
+        public Rectangle(double side1, double side2) //Конструктор с параметрами
+        {
+            this.side1 = side1;
+            this.side2 = side2;
+        }
+        public double AreaCalculator() //Площадь прямоугольника
+        {
+            return side1 * side2;
+        }
+        public double PerimeterCalculator() //Периметр прямоугольника
+        {
+            return 2 * (side1 + side2);
+        }
+        public double Area//Свойство для площади прямоугольника
+        {
+            get { return AreaCalculator(); }
+        }
+
+        public double Perimeter //Свойство для периметра прямоугольника
+        {
+            get { return PerimeterCalculator(); }
+        }
+    }
+}
