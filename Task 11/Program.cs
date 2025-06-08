c5581ca594f7cdd016e3f2784732dba18bf87c8f
Limonchik-D <DavLimon4@yandex.com>
2025-06-08 19:46:51 +0300

    Add files via upload

---
+++ Task 11/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+             Console.WriteLine("Введите ключ доступа (pro, exp) или нажмите Enter для бесплатной версии:");
+              string key = Console.ReadLine();
+
+                DocumentWorker worker;
+
+                if (key.ToLower() == "pro")
+                {
+                    worker = new ProDocumentWorker();
+                }
+                else if (key.ToLower() == "exp")
+                {
+                    worker = new ExpertDocumentWorker();
+                }
+                else
+                {
+                    worker = new DocumentWorker();
+                }
+
+                worker.OpenDocument();
+                worker.EditDocument();
+                worker.SaveDocument();
+
+                Console.ReadLine();
+            
+        }
+    }
+}
