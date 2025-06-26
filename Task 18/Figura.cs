8c8adcfaf9052bd309e4c7a8445970faa9356ef4
Давид Махвиладзе <DavLimon4@yandex.com>
2025-06-26 19:57:59 +0300

    Add files via upload

---
+++ Task 18/Figura.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_18
+{
+    internal class Figura
+    {
+        public void ShowArea(double side)
+        {
+            double area = side * side;
+            Console.WriteLine($"Квадрат, площадь: {area}");
+        }
+
+        public void ShowArea(double length, double width)
+        {
+            double area = length * width;
+            Console.WriteLine($"Прямоугольник, площадь: {area}");
+        }
+
+        public void ShowArea(double a, double b, double h)
+        {
+            double area = (a + b) * h / 2;
+            Console.WriteLine($"Трапеция, площадь: {area}");
+        }
+    }
+}
---
+++ Task 18/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Figura figura = new Figura();
+
+            figura.ShowArea(5);              // Квадрат
+            figura.ShowArea(4, 6);           // Прямоугольник
+            figura.ShowArea(3, 4, 2);        // Трапеция
+        }
+    }
+}
