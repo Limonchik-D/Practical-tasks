0831a488eef1a25a875baad75ab680f004e630bb
Давид Махвиладзе <DavLimon4@yandex.com>
2025-06-26 16:07:28 +0300

    Add files via upload

---
+++ Task 13/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите тип документа (xml, txt, doc): ");
+            string input = Console.ReadLine()?.ToLower();
+
+            AbstractHandler handler;
+
+            switch (input)
+            {
+                case "xml":
+                    handler = new XMLHandler();
+                    break;
+                case "txt":
+                    handler = new TXTHandler();
+                    break;
+                case "doc":
+                    handler = new DOCHandler ();
+                    break;
+                default:
+                    Console.WriteLine("Неизвестный формат документа.");
+                    return;
+            }
+
+            handler.Open();
+            handler.Create();
+            handler.Change();
+            handler.Save();
+
+            Console.ReadLine();
+        }
+    }
+    
+}
