a5f89bdc6836fee34bbc02646a2a83740a17bd0f
Давид Махвиладзе <DavLimon4@yandex.com>
2025-06-26 16:24:28 +0300

    Add files via upload

---
+++ Task 15/MyClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_15
+{
+    internal class MyClass
+    {
+        public void ShowInterfaceA()
+        {
+            Console.WriteLine("Метод ShowInterfaceA() интерфейса InterfaceA");
+        }
+
+        public void ShowInterfaceB()
+        {
+            Console.WriteLine("Метод ShowInterfaceB() интерфейса InterfaceB");
+        }
+    }
+}
