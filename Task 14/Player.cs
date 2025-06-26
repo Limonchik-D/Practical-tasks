2d377957c90b05e8e011e2a71984d5ea5e43171d
Limonchik-D <DavLimon4@yandex.com>
2025-06-26 14:47:20 +0300

    Add files via upload

---
+++ Task 14/Player.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_14
+{
+    internal class Player : IPlayable, IRecodable
+    {
+        public void Play()
+        {
+            Console.WriteLine("Воспроизведение началось.");
+        }
+
+        void IPlayable.Pause()
+        {
+            Console.WriteLine("Воспроизведение приостановлено.");
+        }
+
+        void IPlayable.Stop()
+        {
+            Console.WriteLine("Воспроизведение остановлено.");
+        }
+
+        public void Record()
+        {
+            Console.WriteLine("Запись началась.");
+        }
+
+        void IRecodable.Pause()
+        {
+            Console.WriteLine("Запись приостановлена.");
+        }
+
+        void IRecodable.Stop()
+        {
+            Console.WriteLine("Запись остановлена.");
+        }
+    }
+}
---
+++ Task 14/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player();
+
+            Console.WriteLine("IPlayable");
+            IPlayable playable = player;
+            playable.Play();
+            playable.Pause();
+            playable.Stop();
+
+            Console.WriteLine("\nIRecodable");
+            IRecodable recodable = player;
+            recodable.Record();
+            recodable.Pause();
+            recodable.Stop();
+        }
+    }
+}
