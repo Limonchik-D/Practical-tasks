83e0ea5f6ad2cc5f6095ec71f7cbb7d2d645f98b
Давид Махвиладзе <DavLimon4@yandex.com>
2025-06-26 20:04:27 +0300

    Add files via upload

---
+++ Task 20/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_20
+{
+    internal struct Book
+    {
+        public string Title;      // Название
+        public string Author;     // Ф.И.О. автора
+        public int Year;          // Год издания
+        public int Pages;         // Количество страниц
+
+        // Конструктор
+        public Book(string title, string author, int year, int pages)
+        {
+            Title = title;
+            Author = author;
+            Year = year;
+            Pages = pages;
+        }
+
+        // Метод вывода информации
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Название: {Title}");
+            Console.WriteLine($"Автор: {Author}");
+            Console.WriteLine($"Год издания: {Year}");
+            Console.WriteLine($"Страниц: {Pages}");
+            Console.WriteLine(); 
+        }
+    }
+}
---
+++ Task 20/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book[] books = new Book[3];
+
+            // Инициализируем книги
+            books[0] = new Book("Война и мир", "Лев Толстой", 1865, 1225);
+            books[1] = new Book("1984", "Джордж Оруэлл", 1949, 328);
+            books[2] = new Book("Гарри Поттер", "Дж. К. Роулинг", 1997, 400);
+
+            // Выводим информацию о каждой книге
+            foreach (Book book in books)
+            {
+                book.DisplayInfo();
+            }
+        }
+    }
+}
